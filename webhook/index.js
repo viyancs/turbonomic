b3823ed002f434a3d35071ab16a95aeaea79b8e9
viyancs <msofyancs@gmail.com>
2023-03-20 10:20:40 +0700

    fresh code

---
+++ webhook/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+  console.log('Webhook received:', req.body);
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Webhook server listening at http://localhost:${port}`);
+});
