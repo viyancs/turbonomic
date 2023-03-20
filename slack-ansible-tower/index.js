c029dcc36f9ef1f7feb59dc9e5c6f63ef3180f55
viyancs <msofyancs@gmail.com>
2023-03-20 20:01:41 +0700

    update add bolt js to receive message from slack

---
+++ slack-ansible-tower/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const { App, subtype } = require('@slack/bolt');
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  ignoreSelf:false,
+  // Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
+  // you still need to listen on some port!
+  port: process.env.PORT || 3000
+});
+
+app.message(subtype('bot_message'), ({ event, logger }) => {
+    console.log(event);
+    console.log(event.blocks[5].elements)
+});
+
+(async () => {
+  // Start your app
+  await app.start();
+
+  console.log('⚡️ Bolt app is running!');
+})();
