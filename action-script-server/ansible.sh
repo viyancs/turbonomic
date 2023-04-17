0faf221ceb23d4a3d533e59650a10c27bba25477
viyancs <msofyancs@gmail.com>
2023-04-17 08:37:57 +0700

    adding action script server

--- action-script-server/ansible.sh
+++ action-script-server/ansible.sh
@@ -10,8 +10,19 @@ echo "Executing script..."
 echo "$(date): Script executed." >> $LOG_FILE
 
 myActionData=$(cat -)
-
+# you can use  this data and make logic for decide ansible tower api
 echo "$myActionData" >> $LOG_FILE
 
+TOWER_HOST="https://your.tower.host.com"
+TOWER_USERNAME="your-username"
+TOWER_PASSWORD="your-password"
+WORKFLOW_ID="1234"
 
-#echo "$1" >> $LOG_FILE
+# Get authentication token
+WORKFLOW=$(curl -s -k -X POST \
+  "${TOWER_HOST}/api/v2/workflow_job_templates/${WORKFLOW_ID}/launch/" \
+  -H "Content-Type: application/json" \
+  -d "{\"username\":\"${TOWER_USERNAME}\",\"password\":\"${TOWER_PASSWORD}\"}"
+
+
+echo "$WORKFLOW" >> $LOG_FILE
